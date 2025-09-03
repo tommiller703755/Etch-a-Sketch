b401a9158f5c07c45d47bb0f14829a7b76c7fd8a
tommiller703755 <tom.miller703755@gmail.com>
2025-09-03 21:44:55 +0000

    Add rave-mode, a reset button, and variable grids
    this commit adds rave-mode a togglable made that allows the player to
    create squares of random colors, a reset button that refreshes the grid
    and code adjustments to the create grid function that allows grids of
    varying sizes to be created

--- script.js
+++ script.js
@@ -1,26 +1,65 @@
 // initial setup, create a variable for the container and create divs
 const main_container = document.getElementById("main-container");
 
-for (let i = 0; i < 16; i++) {
-    let column = document.createElement("div");
-    column.classList.add('column')
+let grid_size = 16;
+let rave_mode = false;
+let cell_size = 0;
 
-    for (let j = 0; j < 16; j++) {
-        let div = document.createElement("div");
-        div.classList.add("cell")
-        column.append(div);
+function create_grid() {
+    if (grid_size > 100) {
+        grid_size = 100;
     }
-    
-    main_container.append(column);
+
+    cell_size = 960 / grid_size;
+
+    for (let i = 0; i < grid_size; i++) {
+        let column = document.createElement("div");
+        column.classList.add('column')
+
+        for (let j = 0; j < grid_size; j++) {
+            let div = document.createElement("div");
+            div.classList.add("cell")
+            div.style.width = cell_size.toString() + "px";
+            div.style.height = cell_size.toString() + "px";
+            column.append(div);
+        }
+        
+        main_container.append(column);
+    }
+
+    // setup the event listener
+    const cells = document.querySelectorAll(".cell");
+    cells.forEach(cell => {
+        cell.addEventListener("mouseenter", function() {
+            console.log("cell activated");
+            if (rave_mode) {
+                let rgb = "#" + Math.floor(Math.random() * 16777215).toString(16).padStart(6, "0");
+                
+                cell.style.backgroundColor = rgb;
+            } else {
+                cell.style.backgroundColor = "white";
+            }
+        })
+    });
 }
+create_grid(16);
 
 console.log("Grid creation code created.")
 
-// Detect when a grid cell is hovered over
-const cells = document.querySelectorAll(".cell");
-cells.forEach(cell => {
-    cell.addEventListener("mouseenter", function() {
-        console.log("cell activated.");
-        cell.style.color = "white";
-    })
-})
+// Function to reset the grid
+const reset_button = document.getElementById("reset-button");
+reset_button.addEventListener("click", function() {
+    let columns = document.querySelectorAll(".column");
+    
+    for (let i = 0; i < grid_size; i++) {
+        columns[i].remove();
+    }
+
+    grid_size = prompt("How large should the grid be?", 16);
+    create_grid()
+});
+
+const rave_button = document.getElementById("rave-button");
+rave_button.addEventListener("click", function() {
+    rave_mode = !rave_mode;
+});
